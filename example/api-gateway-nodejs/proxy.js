514c02ccfd2df0d3f0036e86012d5bf51bb3d8be
ldinhtrieu <ldinhtrieu@gmail.com>
2024-02-18 18:02:49 +0700

    add proxy and routes

---
+++ example/api-gateway-nodejs/proxy.js
@@ -0,0 +1,7 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const setupProxies = (app, routes) => {
+  routes.forEach((r) => {
+    app.use(r.url, createProxyMiddleware(r.proxy));
+  });
+};
+exports.setupProxies = setupProxies;
---
+++ example/api-gateway-nodejs/routes.js
@@ -0,0 +1,33 @@
+const ROUTES = [
+  {
+    url: "/free",
+    auth: false,
+    creditCheck: false,
+    rateLimit: {
+      windowMs: 15 * 60 * 1000,
+      max: 5,
+    },
+    proxy: {
+      target: "https://www.google.com",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/free
+    `]: "",
+      },
+    },
+  },
+  {
+    url: "/premium",
+    auth: true,
+    creditCheck: true,
+    proxy: {
+      target: "https://www.google.com",
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/premium
+    `]: "",
+      },
+    },
+  },
+];
+exports.ROUTES = ROUTES;
