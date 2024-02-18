ff21a28542a56dd8a7a036a7a7086f8fa932d1e3
ldinhtrieu <ldinhtrieu@gmail.com>
2024-02-18 21:30:07 +0700

    add credit check

---
+++ example/api-gateway-nodejs/creditcheck.js
@@ -0,0 +1,26 @@
+const checkCredit = (request) => {
+  return new Promise((resolve, reject) => {
+    console.log("Checking credit with token", req.headers["authorization"]);
+    setTimeout(() => {
+      reject("No sufficient credits");
+    }, 500);
+  });
+};
+
+const setupCreditCheck = (app, routes) => {
+  routes.forEach((r) => {
+    if (r.creditCheck) {
+      app.use(r.url, function (req, res, next) {
+        checkCredit(req)
+          .then(() => {
+            next();
+          })
+          .catch((error) => {
+            res.status(402).send({ error });
+          });
+      });
+    }
+  });
+};
+
+exports.setupCreditCheck = setupCreditCheck;
--- example/api-gateway-nodejs/server.js
+++ example/api-gateway-nodejs/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { setupRateLimit } = require("./ratelimit");
 const { setupLogging } = require("./logging");
 const { ROUTES } = require("./routes");
+const { setupCreditCheck } = require("./creditcheck");
 const { setupProxies } = require("./proxy");
 const { setupAuth } = require("./auth");
 const app = express();
@@ -11,6 +12,7 @@ setupLogging(app);
 setupRateLimit(app, ROUTES);
 //setup Auth, #Note: before setup proxies
 setupAuth(app, ROUTES);
+setupCreditCheck(app, ROUTES);
 setupProxies(app, ROUTES);
 
 app.listen(port, () => {
