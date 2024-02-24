2dc486c45c1b75628b0c8cb788217c7dd9ea15a6
ldinhtrieu <ldinhtrieu@gmail.com>
2024-02-24 22:39:01 +0700

    change name

---
+++ example/Nodejs/RabbitMQ-hello-world/send.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+//require the library
+var amqp = require("amqplib/callback_api");
+require("dotenv").config();
+const host = process.env.RABBIT_HOST;
+//then connect to RabbitMQ server
+amqp.connect(`amqp://${host}`, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  //Next we create a channel, which is where most of the API for getting things done resides:
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    var queue = "hello";
+    var msg = "Hello World!1";
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+    channel.sendToQueue(queue, Buffer.from(msg));
+
+    console.log(" [x] Sent %s", msg);
+  });
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
